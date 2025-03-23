92014f7f063b7361a8feb711385ddd65af564950
yeshn13 <yeshnkarosaki@gmail.com>
2025-03-23 23:52:10 +0800

    new version

---
+++ Getting Functional/function.js
@@ -0,0 +1,32 @@
+// var dogName = "dogstyleeee";
+// var dogWeight = 18;
+// if (dogWeight > 20) {
+//     console.log(dogName + " says, WOOF WOOF")
+// } else {
+//     console.log(dogName + " says, woof woof")
+// }
+
+// var dogName = "kalbo";
+// var dogWeight = 22;
+// if (dogWeight > 20) {
+//     console.log(dogName + " says, WOOF WOOF")
+// } else {
+//     console.log(dogName + " says, woof woof")
+// }
+
+// var dogName = "yeshn";
+// var dogWeight = 12;
+// if (dogWeight > 20) {
+//     console.log(dogName + " says, WOOF WOOF")
+// } else {
+//     console.log(dogName + " says, woof woof")
+// }
+
+// var dogName = "lebron";
+// var dogWeight = 30;
+// if (dogWeight > 20) {
+//     console.log(dogName + " says, WOOF WOOF")
+// } else {
+//     console.log(dogName + " says, woof woof")
+// }
+
