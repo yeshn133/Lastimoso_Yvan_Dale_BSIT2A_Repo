dd6193d832f422918bfa5dab600ab2c86ac13ce9
yeshn13 <yeshnkarosaki@gmail.com>
2025-03-23 23:53:24 +0800

    new version

--- Getting Functional/function.js
+++ Getting Functional/function.js
@@ -30,3 +30,16 @@
 //     console.log(dogName + " says, woof woof")
 // }
 
+function bark(name, weight) {
+    if (weight > 20) {
+        console.log(name + " says WOOF WOOF!")
+    } else {
+        console.log(name + " says woof woof!")
+    }
+}
+
+bark("lebron", 35);
+bark("dogstyleeee", 19);
+bark("kalbo", 40);
+bark("yeshn", 26);
+bark("elay", 45);
